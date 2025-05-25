929dfceeb34aa3e2989ada316be77cee521f1ee7
WinterMute <WinterMute@users.noreply.github.com>
2025-05-25 13:59:09 +0000

    Deploying to gh-pages from @ devkitPro/wut@5fdc7ffe85e971c537e1a6b7392c6f9686523257 🚀

---
+++ search/all_b.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['japan_0',['japan',['../namespacenn_1_1erreula.html#aa6498d628b446c18e1c5087341741ec2a53a577bb3bc587b0c28ab808390f1c9b',1,'nn::erreula::Japan'],['../namespacenn_1_1swkbd.html#aa6498d628b446c18e1c5087341741ec2a53a577bb3bc587b0c28ab808390f1c9b',1,'nn::swkbd::Japan']]],
+  ['japanese_1',['japanese',['../namespacenn_1_1sl.html#a315ca917ad583797f709ea477dd28705a3443f3a583d4c3bf18c275fce5197bf4',1,'nn::sl::Japanese'],['../namespacenn_1_1erreula.html#aa5868269c7aa0334a18bb7f8063bf885af32ced6a9ba164c4b3c047fd1d7c882e',1,'nn::erreula::Japanese'],['../namespacenn_1_1swkbd.html#ada735c6d6b04a8714b2751f0c4053e34af32ced6a9ba164c4b3c047fd1d7c882e',1,'nn::swkbd::Japanese']]],
+  ['join_5fgame_5fid_2',['join_game_id',['../group__nn__acp__title.html#a3dcaed87bd4dbd6551aae88c1f310e7b',1,'ACPMetaXml']]],
+  ['join_5fgame_5fmode_5fmask_3',['join_game_mode_mask',['../group__nn__acp__title.html#aba498b209ca442bdf1aba38ee7ed0b3e',1,'ACPMetaXml']]],
+  ['joinavailabilityflag_4',['joinAvailabilityFlag',['../group__nn__fp__fp.html#ga80960aecdad484a305ec5c62537672a9',1,'nn::fp::GameMode']]],
+  ['joingameid_5',['joinGameId',['../group__nn__fp__fp.html#gac3b22256e0a806b627e04949b0cc6367',1,'nn::fp::GameMode']]],
+  ['joingamemode_6',['joinGameMode',['../group__nn__fp__fp.html#ga4b9949efbcac4bcbe82546515da28d98',1,'nn::fp::GameMode']]],
+  ['joingroupid_7',['joinGroupId',['../group__nn__fp__fp.html#ga89541b0c7b76eb34401cfa9b60a7d66f',1,'nn::fp::GameMode']]],
+  ['joinqueue_8',['joinQueue',['../group__coreinit__thread.html#ae12b5a541aff405df56ed7abfa97e3b4',1,'OSThread']]],
+  ['journalfreespacesize_9',['journalFreeSpaceSize',['../filesystem__fsa_8h.html#aa774a22b477ab12e6e8761816865a95a',1,'FSAResponseGetInfoByQuery.__unnamed9__']]],
+  ['jump_10',['Jump',['../namespacenn_1_1erreula.html#aadc28b683bb08ed2bf55d90f7144e198',1,'nn::erreula']]],
+  ['jumptitleinfoserializer_11',['jumpTitleInfoSerializer',['../namespacenn_1_1sl_1_1details.html#a4c539779bbef66b0c667c3db036139e6',1,'nn::sl::details::CacheManagerInternal']]],
+  ['jumpto_12',['jumpTo',['../group__sysapp__launch.html#a1a1f97891adc04135479fd75a33f73f7',1,'SysAppSettingsArgs']]]
+];
---
+++ search/groups_11.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['raw_20hid_20data_20of_20the_20drcs_0',['Raw HID data of the DRCs',['../group__nsysccr__hid.html',1,'']]],
+  ['registers_1',['Registers',['../group__gx2__registers.html',1,'']]],
+  ['rendezvous_2',['Rendezvous',['../group__coreinit__rendezvous.html',1,'']]],
+  ['report_3',['COS Report',['../group__coreinit__cosreport.html',1,'']]],
+  ['resource_4',['Resource',['../group__gx2r__resource.html',1,'']]],
+  ['result_5',['result',['../group__nn__acp__result.html',1,'Result'],['../group__nn__result.html',1,'Result'],['../group__sndcore2__result.html',1,'Result']]],
+  ['rpl_20interface_6',['rpl interface',['../group__erreula__rpl.html',1,'RPL Interface'],['../group__swkbd__rpl.html',1,'RPL Interface']]]
+];
